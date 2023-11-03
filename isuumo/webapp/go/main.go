042cae70fbdc9744d0e243d2e8b11a3d7dfa5efd
mackee <macopy123@gmail.com>
2023-11-03 15:52:07 +0900

    chairをbulk insert化

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -388,13 +388,8 @@ func postChair(c echo.Context) error {
 	}
 
 	ctx := c.Request().Context()
-	tx, err := db.BeginTx(ctx, nil)
-	if err != nil {
-		c.Logger().Errorf("failed to begin tx: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	defer tx.Rollback()
-	for _, row := range records {
+	chairs := make([]Chair, len(records))
+	for i, row := range records {
 		rm := RecordMapper{Record: row}
 		id := rm.NextInt()
 		name := rm.NextString()
@@ -413,14 +408,25 @@ func postChair(c echo.Context) error {
 			c.Logger().Errorf("failed to read record: %v", err)
 			return c.NoContent(http.StatusBadRequest)
 		}
-		_, err := tx.ExecContext(ctx, "INSERT INTO chair(id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)", id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock)
-		if err != nil {
-			c.Logger().Errorf("failed to insert chair: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
+		chairs[i] = Chair{
+			ID:            int64(id),
+			Name:          name,
+			Description:   description,
+			Thumbnail:     thumbnail,
+			Price:         int64(price),
+			Height:        int64(height),
+			Width:         int64(width),
+			Depth:         int64(depth),
+			Color:         color,
+			Features:      features,
+			Kind:          kind,
+			Popularity:    int64(popularity),
+			Stock:         int64(stock),
+			FeaturesArray: features,
 		}
 	}
-	if err := tx.Commit(); err != nil {
-		c.Logger().Errorf("failed to commit tx: %v", err)
+	if _, err := db.NamedExecContext(ctx, "INSERT INTO chair(id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock) VALUES(:id, :name, :description, :thumbnail, :price, :height, :width, :depth, :color, :features, :kind, :popularity, :stock)", chairs); err != nil {
+		c.Logger().Errorf("failed to insert chair: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 	return c.NoContent(http.StatusCreated)
